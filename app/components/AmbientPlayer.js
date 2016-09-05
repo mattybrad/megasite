1ea85c487badf734a59a431ab1b843cd3430645a
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2016-09-05 22:01:23 +0100

    nice audio scheduling

--- app/components/AmbientPlayer.js
+++ app/components/AmbientPlayer.js
@@ -8,26 +8,70 @@ class Channel {
     this.type = type;
     this.params = params;
     this.actx = actx;
+    this.current16thNote = 0;
+    this.tempo = 120;
+    this.lookahead = 25.0;
+    this.scheduleAheadTime = 0.1;
+    this.nextNoteTime = 0.0;
+
     this.outputNode = this.actx.createGain();
     this.outputNode.connect(this.actx.destination);
     this.outputNode.gain.value = 0.1;
+
+    this.scheduler();
+  }
+
+  scheduler() {
+    while(this.nextNoteTime < this.actx.currentTime + this.scheduleAheadTime) {
+      this.scheduleNote(this.current16thNote, this.nextNoteTime);
+      this.nextNote();
+    }
+    setTimeout(this.scheduler.bind(this), this.lookahead);
+  }
+
+  scheduleNote(beatNumber, time) {
+    //currentNoteStartTime = time;
+    // create an oscillator
+    var osc = this.actx.createOscillator();
+    osc.connect(this.outputNode);
+
+    if (! (beatNumber % 16) )         // beat 0 == low pitch
+    osc.frequency.value = 220.0;
+    else if (beatNumber % 4)          // quarter notes = medium pitch
+    osc.frequency.value = 1000 * Math.random();
+    else                              // other 16th notes = high pitch
+    osc.frequency.value = 880.0;
+    osc.start( time );
+    osc.stop( time + 0.2 );
+  }
+
+  nextNote() {
+    // Advance current note and time by a 16th note...
+    var secondsPerBeat = 60.0 / this.tempo;	// picks up the CURRENT tempo value!
+    this.nextNoteTime += 0.25 * secondsPerBeat;	// Add 1/4 of quarter-note beat length to time
+
+    this.current16thNote++;	// Advance the beat number, wrap to zero
+    if (this.current16thNote == 16) {
+      this.current16thNote = 0;
+    }
+    console.log(this.current16thNote)
   }
 
   scheduleSounds() {
-    var oscillator;
-    oscillator = this.actx.createOscillator();
-    oscillator.type = this.params.oscType;
-    oscillator.frequency.value = 300;
-    oscillator.connect(this.outputNode);
-    oscillator.start(this.actx.currentTime);
-    oscillator.stop(this.actx.currentTime + 0.05);
-
-    oscillator = this.actx.createOscillator();
-    oscillator.type = this.params.oscType;
-    oscillator.frequency.value = 600;
-    oscillator.connect(this.outputNode);
-    oscillator.start(this.actx.currentTime + 0.5);
-    oscillator.stop(this.actx.currentTime + 0.7);
+    // var oscillator;
+    // oscillator = this.actx.createOscillator();
+    // oscillator.type = this.params.oscType;
+    // oscillator.frequency.value = 300;
+    // oscillator.connect(this.outputNode);
+    // oscillator.start(this.actx.currentTime);
+    // oscillator.stop(this.actx.currentTime + 0.05);
+    //
+    // oscillator = this.actx.createOscillator();
+    // oscillator.type = this.params.oscType;
+    // oscillator.frequency.value = 600;
+    // oscillator.connect(this.outputNode);
+    // oscillator.start(this.actx.currentTime + 0.5);
+    // oscillator.stop(this.actx.currentTime + 0.7);
 
     // need to schedule things properly, not based on setInterval
   }
@@ -75,7 +119,7 @@ class AmbientPlayerComponent extends React.Component {
   scheduleAllSounds(tempChannel) {
     // will map over all active channels
     if(this.props.active) {
-      tempChannel.scheduleSounds();
+      //tempChannel.scheduleSounds();
     }
   }
 
--- app/pages/Home.js
+++ app/pages/Home.js
@@ -7,7 +7,24 @@ export default class PageComponent extends React.Component {
     return (
       <div>
         <BackgroundDefinition primaryColor='#00cccc' secondaryColor='#0000ff' />
-        <AmbientDefinition freq={220} />
+        <AmbientDefinition
+          channels={
+            [
+              {
+                type: "oscSet",
+                notes: [400,450,500,800],
+                wave: "sine",
+                probability: 0.5
+              },
+              {
+                type: "oscSet",
+                notes: [200,250],
+                wave: "square",
+                probability: 1
+              }
+            ]
+          }
+        />
         <h1>{"Matt's website"}</h1>
         <p>My name is Matt and this is my website.</p>
         <p>The site contains text and sound and pictures and information and thoughts, but the container is itself formed of these things, in a way that could be considered gently meta.</p>
